fe30d71595872708cc7c57aabdf9ad4ff791ef9e
goldbek87-stack <goldbek87@gmail.com>
2026-09-05 18:52:22 +0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uz.hisobchi.app',
+  appName: 'Hisobchi',
+  webDir: 'www'
+};
+
+export default config;
