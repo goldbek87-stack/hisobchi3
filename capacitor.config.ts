8a0ae627d838c05d673984e793b5e89fcd97cc79
goldbek87-stack <goldbek87@gmail.com>
2026-09-05 19:07:29 +0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'uz.hisobchi.app',
-  appName: 'Hisobchi',
-  webDir: 'www'
-};
-
-export default config;
